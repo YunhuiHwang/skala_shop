bd0b35c4a374deaae6cf5d1589595b63ae039992
황윤희 <dinojelly323@naver.com>
2026-07-30 11:39:52 +0900

    chore: initial project setup (Gradle, application.yml)

---
+++ src/main/java/com/skala/shop/SkalaShopApiApplication.java
@@ -0,0 +1,13 @@
+package com.skala.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkalaShopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkalaShopApiApplication.class, args);
+	}
+
+}
